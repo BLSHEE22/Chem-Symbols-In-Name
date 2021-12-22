5cd23a8f60f8c220c3643499f2d1deb1be3bf0cd
Brendan Sheedy <60196992+BLSHEE22@users.noreply.github.com>
2021-12-22 13:12:59 -0500

    Update symbol_test.py

--- symbol_test.py
+++ symbol_test.py
@@ -1,3 +1,31 @@
+# AMC's Breaking Bad does a very interesting thing, and I wanted to explore it further.
+
+# Each name of the credits has a group of highlighted letters, and these letters make up a various chemical symbol.
+
+# For a name to contain a chemical symbol, it must meet ANY of the following criteria:
+# - Contain an "a" "b", "c", "f", "h", "i", "k", "n", "o", "p", "s", "u", "v", "w", or "y".
+# - Contain an "er", "gd", "ge", "lr", "md", "mg", "mt", "re", "rg", "te", "tl", "tm", "xe", or "zr".
+
+# So, a name containing no chemical symbols must consist of ONLY the letters:
+# --> "d", "e", "g", "j", "l", "m", "q", "r", "t", "x", or "z".
+# AND that name must not contain any of the above two-letter symbols.
+# For example, "Gex" would not work because it contains symbol "Ge".
+# Conversely, "Lex" or "Mel" would work.
+# There are very few full names in existence that follow the specifications immediately above.
+# (hence why Breaking Bad had no trouble with this process!)
+
+# What if we wanted to know just how many chemical symbols existed within a name?
+
+# The below program can be run with the following command:
+# python brba.py name
+
+# It will print a summary of the symbols found, and then print the name with the earliest symbol
+# highlighted in green, as if the name were a cast/crew member of AMC's Breaking Bad.
+
+# My challenge to you would be to find a legal full name that doesn't contain a chemical symbol.
+
+# Good luck, and have fun!
+
 import sys
 
 def main():
@@ -45,7 +73,7 @@ def main():
     print
 
     if len(finds) == 0:
-        print("Sorry, there's no chemical symbol contained within " + name + ".")
+        print("Sorry, there's no chemical symbol contained within " + name0 + ".")
     elif len(finds) == 1:
         print("There's only one chemical symbol contained in " + name0 + "!")
         print(output)
